51a0bba4a15bbfdb51eb6b5a79d89b15bde96ff0
Taijing Chen <taijingchen99@gmail.com>
2021-10-01 00:11:14 -0500

    lua tuning

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -17,17 +17,23 @@ init_r = 0
 GAMMA = 0.02;
 SENSOR_STD_DEV   = 0.5;
 D_SHORT = SENSOR_STD_DEV * 1.5;
-D_LONG = SENSOR_STD_DEV * 2;
-P_OUTSIDE_RANGE = 0.0;
+D_LONG = SENSOR_STD_DEV * 2.5;
+P_OUTSIDE_RANGE = 1e-4;
 
-MOTION_X_STD_DEV = 0.01;
-MOTION_Y_STD_DEV = 0.01;
+MOTION_X_STD_DEV = 0.15;
+MOTION_Y_STD_DEV = 0.15;
 MOTION_A_STD_DEV = 0.01;
 
-MOTION_DIST_K1 = 0.7;
+MOTION_DIST_K1 = 1.0;
 MOTION_DIST_K2 = 0.5;
-MOTION_A_K1 = 1.5;
+MOTION_A_K1 = 0.5;
 MOTION_A_K2 = 1.5;
 
 -- 22-44 go-straight --> turn --> go-straight
--- 25-18 go-straight
+-- 23-38 go-straight --> turn --> go-straight (hard)
+-- 25-18 go-straight (hard)
+-- 27-15 go-straight
+-- 09-24 go back and forth
+-- 08-55 go back and forth
+-- 14-01 go bach and forth
+
