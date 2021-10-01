190207606cb20c860dad6d142bbaf7e2563ca67a
Taijing Chen <taijingchen99@gmail.com>
2021-10-01 00:08:54 -0500

    lua tuning

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -2,8 +2,32 @@ map = "maps/GDC1.txt"
 init_x = 14.7
 init_y = 14.24
 init_r = 0
+
+-- simulator
+-- GAMMA = 0.5;
+-- MOTION_X_STD_DEV = 0.07;
+-- MOTION_Y_STD_DEV = 0.02;
+-- MOTION_A_STD_DEV = 0.02;
+-- SENSOR_STD_DEV   = 0.05;
+-- D_SHORT = 0.2
+-- D_LONG = 0.1
+-- P_OUTSIDE_RANGE = 0.0
+
+-- log data
 GAMMA = 0.02;
-MOTION_X_STD_DEV = 0.1;
-MOTION_Y_STD_DEV = 0.05;
-MOTION_A_STD_DEV = 0.1;
 SENSOR_STD_DEV   = 0.5;
+D_SHORT = SENSOR_STD_DEV * 1.5;
+D_LONG = SENSOR_STD_DEV * 2;
+P_OUTSIDE_RANGE = 0.0;
+
+MOTION_X_STD_DEV = 0.01;
+MOTION_Y_STD_DEV = 0.01;
+MOTION_A_STD_DEV = 0.01;
+
+MOTION_DIST_K1 = 0.7;
+MOTION_DIST_K2 = 0.5;
+MOTION_A_K1 = 1.5;
+MOTION_A_K2 = 1.5;
+
+-- 22-44 go-straight --> turn --> go-straight
+-- 25-18 go-straight
