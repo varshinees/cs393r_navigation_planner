36f5713f062e035f63355317080b160e4fe22a54
Taijing Chen <taijingchen99@gmail.com>
2021-10-11 10:13:01 -0500

    final version

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -15,25 +15,25 @@ init_r = 0
 
 -- log data
 GAMMA = 0.07;
-SENSOR_STD_DEV = 0.6;
+SENSOR_STD_DEV = 0.1;
 D_SHORT = SENSOR_STD_DEV * 1.5; --TODO
 D_LONG = SENSOR_STD_DEV * 2.0;
 P_OUTSIDE_RANGE = 1e-4;
 
 MOTION_X_STD_DEV = 0.1;
-MOTION_Y_STD_DEV = 0.1;
+MOTION_Y_STD_DEV = 0.05;
 MOTION_A_STD_DEV = 0.1;
 
-MOTION_DIST_K1 = 0.7;
-MOTION_DIST_K2 = 0.15;
-MOTION_A_K1 = 0.3;
-MOTION_A_K2 = 2.0;
+MOTION_DIST_K1 = 0.3;
+MOTION_DIST_K2 = 0.05;
+MOTION_A_K1 = 0.1;
+MOTION_A_K2 = 1.0;
 
 MAX_D_DIST = 0.15
 MAX_D_ANGLE = 3.1415926 / 18
 
 -- 22-44 go-straight --> turn --> go-straight
--- 23-38 go-straight --> turn --> go-straight (hardest)
+-- 23-38 u-turn (hardest)
 -- 25-18 go-straight (hard)
 -- 27-15 go-straight
 -- 09-24 go back and forth
